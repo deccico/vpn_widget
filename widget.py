83b306b920aa61796a3c25de37ffffd91ea873a2
Adrián Deccico <deccico@gmail.com>
2017-04-16 23:07:05 +1000

    fixing status update

--- widget.py
+++ widget.py
@@ -3,6 +3,9 @@
 
 """
 Displays VPN connection status in the System bar and lets user connect / disconnect from vpn
+
+To automatically start this script, please check the link below
+https://askubuntu.com/questions/48321/how-do-i-start-applications-automatically-on-login
 """
 
 import os
@@ -10,6 +13,7 @@ import signal
 import subprocess
 
 import gi
+
 gi.require_version('Gtk', '3.0')
 from gi.repository import AppIndicator3 as appindicator
 from gi.repository import Gtk as gtk
@@ -40,8 +44,8 @@ class Widget:
         item_status = gtk.MenuItem('Disconnect')
         item_status.connect('activate', self.disconnect)
         menu.append(item_status)
-        item_status = gtk.MenuItem('Force Status Update')
-        item_status.connect('activate', self.set_status)
+        item_status = gtk.MenuItem('Update Connection Status')
+        item_status.connect('activate', self.get_status)
         menu.append(item_status)
         item_quit = gtk.MenuItem('Quit')
         item_quit.connect('activate', self.quit)
@@ -50,25 +54,22 @@ class Widget:
         return menu
 
     def main(self):
-        self.indicator = appindicator.Indicator.new(self.APPINDICATOR_ID, self.ICON_OFF, appindicator.IndicatorCategory.SYSTEM_SERVICES)
+        self.indicator = appindicator.Indicator.new(self.APPINDICATOR_ID, self.ICON_OFF,
+                                                    appindicator.IndicatorCategory.SYSTEM_SERVICES)
         self.indicator.set_status(appindicator.IndicatorStatus.ACTIVE)
         self.indicator.set_menu(self.build_menu())
 
-        #This sets the handler for “INT” signal processing
+        # This sets the handler for “INT” signal processing
         #- the one issued by the OS when “Ctrl+C” is typed.
         #The handler we assign to it is the “default” handler, which,
         #in case of the interrupt signal, is to stop execution.
-        signal.signal(signal.SIGINT, signal.SIG_DFL) #listen to quit signal
+        signal.signal(signal.SIGINT, signal.SIG_DFL)  #listen to quit signal
 
         notify.init(self.APPINDICATOR_ID)
         self.update()
         glib.timeout_add_seconds(self.UPDATE_FREQUENCY, self.update)
         gtk.main()
 
-
-    def update(self):
-        self.set_status(None)
-        return True
 
     def quit(self, source):
         if self.timer:
@@ -94,21 +95,32 @@ class Widget:
                                     None).show()
         self.update()
 
-    def set_status(self, _):
-        status = self.get_connection_status()
-        print(status)
+
+    def get_status(self, _):
+        notify.Notification.new("<b>Connection Status</b>", self._update_status(), None).show()
+
+
+    def update(self):
         current_status = self.connection_status
+        status = self._update_status()
+        if current_status != self.connection_status:
+            notify.Notification.new("<b>Connection Status</b>", status, None).show()
+        return True
+
+
+    def _update_status(self):
+        status = self.get_connection_status()
         if self.CONNECTED_STRING == status[0:len(self.CONNECTED_STRING)]:
             self.connection_status = 1
             self.indicator.set_icon(self.ICON_ON)
         else:
             self.connection_status = 0
             self.indicator.set_icon(self.ICON_OFF)
-        if current_status != self.connection_status:
-            notify.Notification.new("<b>Connection Status</b>", status, None).show()
+        return status
 
     def get_connection_status(self):
         return subprocess.check_output(["expressvpn", "status"])
+
 
 if __name__ == "__main__":
     w = Widget()
